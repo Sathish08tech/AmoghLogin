5bf294d66d8695a78bc816b0746856f94e026ce1
Sathish08tech <Amogh@dsatishs-macbook-pro.local>
2016-02-22 00:36:18 +0530

    Profile Page added

---
+++ Amogh login/ProfilePage.h
@@ -0,0 +1,13 @@
+//
+//  ProfilePage.h
+//  Amogh login
+//
+//  Created by Amogh on 22/02/16.
+//  Copyright © 2016 Amogh. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface ProfilePage : NSObject
+
+@end
---
+++ Amogh login/ProfilePage.m
@@ -0,0 +1,13 @@
+//
+//  ProfilePage.m
+//  Amogh login
+//
+//  Created by Amogh on 22/02/16.
+//  Copyright © 2016 Amogh. All rights reserved.
+//
+
+#import "ProfilePage.h"
+
+@implementation ProfilePage
+
+@end
